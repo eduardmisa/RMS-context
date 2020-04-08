8ec9887408cf0d1f3f06a301328ca3e811a75420
EduardMisa <eduardo@tirsolutions.com>
2020-04-08 13:56:32 +0800

    updated configs

---
+++ 0-configuration.sh
@@ -0,0 +1,4 @@
+export DEPLOYMENT_PATH="/var/www/rms-context.u4rdsystem.com"
+export GIT_LINK="https://eduardmisa@github.com/eduardmisa/RMS-context.git"
+export GIT_BRANCH="master"
+export GIT_PASSWORD="531ed96864Z"
--- 01-git-pull-repository.sh
+++ 01-git-pull-repository.sh
@@ -1,7 +1,11 @@
-set password "531ed96864Z"
+set DEPLOYMENT_PATH [lindex $argv 0]
+set GIT_LINK [lindex $argv 1]
+set GIT_BRANCH [lindex $argv 2]
+set GIT_PASSWORD [lindex $argv 3]
 
-cd /var/www/rms-context.u4rdsystem.com
-spawn git pull "https://eduardmisa@github.com/eduardmisa/RMS-context.git" master
+cd $DEPLOYMENT_PATH
+
+spawn git pull "$GIT_LINK" $GIT_BRANCH
 expect "Password for"
-send "$password\n"
+send "$GIT_PASSWORD\n"
 interact
