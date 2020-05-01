7ac999c79269d158adc1b08cc3687f22e55ae539
EduardMisa <eduardo@tirsolutions.com>
2020-05-01 15:53:53 +0800

    removed page size limit of 50

--- applicationlayer/paginator.py
+++ applicationlayer/paginator.py
@@ -10,7 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 class SimplePageNumberPagination(pagination.PageNumberPagination):
     page_query_param = 'page'
     page_size_query_param = 'page-size'
-    max_page_size = 50
+    # max_page_size = 50
     # page_size = 10
 
     invalid_page_message = _('Invalid page.')
