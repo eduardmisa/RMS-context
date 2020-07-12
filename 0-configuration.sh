9d996221637956feced5d9527436a2a60f9561ed
eduardmisa <eduardmisa@gmail.com>
2020-07-12 15:02:22 +0800

    rshrth

--- 0-configuration.sh
+++ 0-configuration.sh
@@ -1,4 +1,3 @@
 export DEPLOYMENT_PATH="/var/www/rms-context.emisa.dev"
-export GIT_LINK="https://eduardmisa@github.com/eduardmisa/RMS-context.git"
+export GIT_LINK="git@github.com:eduardmisa/RMS-context.git"
 export GIT_BRANCH="master"
-export GIT_PASSWORD="531ed96864Z"
--- 01-git-pull-repository.sh
+++ 01-git-pull-repository.sh
@@ -1,11 +1,9 @@
 set DEPLOYMENT_PATH [lindex $argv 0]
 set GIT_LINK [lindex $argv 1]
 set GIT_BRANCH [lindex $argv 2]
-set GIT_PASSWORD [lindex $argv 3]
 
 cd $DEPLOYMENT_PATH
 
 spawn git pull "$GIT_LINK" $GIT_BRANCH
-expect "Password for"
-send "$GIT_PASSWORD\n"
+
 interact
